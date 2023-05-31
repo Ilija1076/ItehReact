7ec8fc9b3757e2378e0354fd8155bf5e73a4f24c
Ilija1076 <messi.kralj00@gmail.com>
2023-05-31 16:27:53 +0200

    Pocetak, navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Navbar from "./components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
+  const Kontakt = () => {
+    const [ime, setIme] = useState("");
+    const [email, setEmail] = useState("");
+
+    const handleImeChange = (e) => {
+      setIme(e.target.value);
+    };
+
+    const handleEmailChange = (e) => {
+      setEmail(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      // Ovde možete implementirati logiku za slanje podataka na server
+    };
+
+    return (
+      <div>
+        <h1>Kontaktirajte nas</h1>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Ime:
+            <input type="text" value={ime} onChange={handleImeChange} />
+          </label>
+          <br />
+          <label>
+            Email:
+            <input type="email" value={email} onChange={handleEmailChange} />
+          </label>
+          <br />
+          <button type="submit">Pošalji</button>
+        </form>
+      </div>
+    );
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      {/* Other components or content */}
     </div>
   );
 }
-
 export default App;
---
+++ src/pages/kontakt.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Kontakt = () => {
+  return (
+    <div>
+      <h1>Kontaktirajte nas</h1>
+      <p>Popunite formular ispod kako biste nas kontaktirali:</p>
+      {/* Ovde možete dodati formu */}
+    </div>
+  );
+};
+
+export default Kontakt;
